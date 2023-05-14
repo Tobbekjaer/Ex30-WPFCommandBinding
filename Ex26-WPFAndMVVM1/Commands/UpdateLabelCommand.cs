ee29d3778671c574792b1e3affe35181ff41b5d6
Tobbekjaer <78278904+Tobbekjaer@users.noreply.github.com>
2023-05-14 13:18:06 +0200

    Add project files.

---
+++ Ex26-WPFAndMVVM1/Commands/UpdateLabelCommand.cs
@@ -0,0 +1,30 @@
+﻿using Ex26_WPFAndMVVM1.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Ex26_WPFAndMVVM1.Commands
+{
+    public class UpdateLabelCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            if(parameter is MainViewModel mvm) {
+                mvm.MyLabelText = DateTime.Now.ToString();
+            }
+            else {
+                throw new ArgumentException("Illegal parameter type.");
+            }
+        }
+    }
+}
---
+++ Ex26-WPFAndMVVM1/ViewModel/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Ex26_WPFAndMVVM1.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Ex26_WPFAndMVVM1.ViewModel
+{
+    // MainViewModel inherits from the INotifyPropertyChanged interface
+    public class MainViewModel : INotifyPropertyChanged
+    {
+		private string myLabelText = "Text not set yet";
+
+		public string MyLabelText
+		{
+			get { return myLabelText; }
+			set { 
+				myLabelText = value;
+				OnPropertyChanged("MyLabelText");
+			}
+		}
+
+		private string myTextBoxText = "Text not set yet";
+
+		public string MyTextBoxText
+		{
+			get { return myTextBoxText; }
+			set { 
+				myTextBoxText = value;
+                OnPropertyChanged("MyTextBoxText");
+            }
+		}
+
+        // Property der inititaliserer en instans af UpdateLabelCommand klassen
+        public ICommand UpdateLabelCommand { get; } = new UpdateLabelCommand();
+
+        // Property der inititaliserer en instans af UpdateTextBoxCommand klassen
+        public ICommand UpdateTextBoxCommand { get; } = new UpdateTextBoxCommand();
+
+        // Implementing OnPropertyChanged helt method for the INotifyPropertyChanged interface
+        public event PropertyChangedEventHandler PropertyChanged;
+		protected void OnPropertyChanged(string propertyName)
+		{
+			PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
+			if(propertyChanged != null) {
+				propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+
+
+
+	}
+}
